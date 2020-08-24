8df6228dc3f77d911ee9ca358a6163be4e129ce7
Iapetus-11 <olimdahacker@gmail.com>
2020-08-24 15:31:53 -0400

    Turn Viewer into an iterator for ease of use

--- video2ascii/Viewer.py
+++ video2ascii/Viewer.py
@@ -5,7 +5,10 @@ import cv2
 class Viewer:
     def __init__(self, meta):
         self.__dict__ = meta
+
+        self.current_frame = 0
         self.pretty_frames = list(map(self._pretty_frame, self.frames))
+        self.end_frame = len(self.pretty_frames)
 
     def _pretty_frame(self, frame):
         return ''.join([f'\n{"".join(row)}' for row in frame])
@@ -18,3 +21,13 @@ class Viewer:
             print(frame)
             time.sleep(1/fps)
             os.system('cls')
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current_frame > self.end_frame:
+            raise StopIteration
+        else:
+            self.current_frame += 1
+            return self.pretty_frames[self.current_frame - 1]
