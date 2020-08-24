7a43a7bf1382bb59279dc9cfab8c7255d2a61059
Iapetus11 <olimdahacker@gmail.com>
2020-08-24 03:13:38 -0400

    remove pygame stuff

--- video2ascii/Viewer.py
+++ video2ascii/Viewer.py
@@ -1,4 +1,3 @@
-import pygame
 import time
 import os
 import cv2
@@ -22,32 +21,3 @@ class Viewer:
             print(frame)
             time.sleep(1/fps)
             os.system('cls')
-
-    def view(self, *, fps=None):
-        fps = self.fps
-
-        pygame.init()
-
-        disp = pygame.display.set_mode((int(self.width), int(self.height),))
-        pygame.display.set_caption('video2ascii')
-
-        white = (255, 255, 255,)
-        black = (0, 0, 0,)
-
-        font = pygame.font.Font('CONSOLA.TTF', 5)
-        for frame in map(self._pretty_frame, self.frames):
-            disp.fill(black)
-
-            text = font.render(frame, True, white, black)
-
-            textRect = text.get_rect()
-            textRect.center = (self.width // 2, self.height // 2,)
-
-            disp.blit(text, textRect)
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit()
-
-            pygame.display.update()
