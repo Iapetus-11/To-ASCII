366d6df6e8fbb7d5409d53cd7cab2f20285da4e0
Iapetus11 <olimdahacker@gmail.com>
2020-08-24 00:41:00 -0400

    finish basic Viewer

--- video2ascii/Viewer.py
+++ video2ascii/Viewer.py
@@ -1,1 +1,26 @@
+import time
 import os
+
+
+class Viewer:
+    def __init__(self, meta):
+        self.__dict__ = meta
+
+    def _pretty_frame(self, frame):
+        body = ''
+
+        for row in frame:
+            body += f'\n{"".join(row)}'
+
+        return body
+
+    def classic_view(self, *, fps=None):
+        frames = map(_pretty_frame, self.frames)
+        fps = self.video.get(cv2.CAP_PROP_FPS)
+
+        for frame in frames:
+            print(frame)
+
+            time.sleep(1/fps)
+
+            os.system('cls')
