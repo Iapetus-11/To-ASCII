037297d269e00700b4260ddd5e0ad45d5d3720f3
Iapetus-11 <olimdahacker@gmail.com>
2020-08-25 17:01:40 -0400

    make asciify_row its own function instead of a lambda

--- video2ascii/Video.py
+++ video2ascii/Video.py
@@ -49,6 +49,9 @@ class Video:
     def asciify_pixel(self, p):  # takes [r, g, b]
         return self.gradient[int((((int(p[0]) + int(p[1]) + int(p[2])) / 3)*(len(self.gradient)-1))/255)]
 
+    def asciify_row(self, row):
+        return map(self.asciify_pixel, row)
+
     def convert(self):
         if self.verbose: print('Converting...')
 
@@ -61,4 +64,4 @@ class Video:
 
             img = cv2.resize(img, (int(img.shape[1]*self.scale*self.w_stretch), int(img.shape[0]*self.scale),))
 
-            self.frames.append(map(lambda row: map(self.asciify_pixel, row), img))
+            self.frames.append(map(self.asciify_row, img))
