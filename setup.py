0420d9d3599bed9b5066f4803069092d17c5b6ed
Iapetus-11 <olimdahacker@gmail.com>
2020-08-26 15:46:26 -0400

    2.0.1 add license to setup.py and update readme

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="to-ascii",
-    version="2.0.0",
-    author="Iapetus-11",
-    description="A package which can convert videos, images, and gifs to ascii art!",
+    name='to-ascii',
+    version='2.0.1',
+    author='Iapetus-11',
+    description='A package which can convert videos, images, and gifs to ascii art!',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Iapetus-11/to-ascii",
+    long_description_content_type='text/markdown',
+    url='https://github.com/Iapetus-11/to-ascii',
     packages=setuptools.find_packages(),
     install_requires=[
         'opencv-python'
     ],
+    data_files = [
+        ('', ['LICENSE'])
+    ],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     python_requires='>=3.6'
 )
