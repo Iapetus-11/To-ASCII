95a492b12b544972bc03f81dcc13834d0232c020
Iapetus-11 <olimdahacker@gmail.com>
2020-08-26 13:58:51 -0400

    rename 2ascii folder to toascii for rewrite

--- 2ascii/Constants.py
+++
@@ -1,6 +0,0 @@
-
-gradients = [  # gradients which can be chosen when creating a video object later
-    ' `~+=!*/\\0G@',
-    ' `~+*/\\!G@',
-    ' `-~+\\!G@'
-]
--- 2ascii/__init__.py
+++
@@ -1,1 +0,0 @@
-from .Video import Video
