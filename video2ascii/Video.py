a7cde2295fbe9e73660a92f524a36f7aefe527e4
Iapetus-11 <olimdahacker@gmail.com>
2020-08-25 22:31:21 -0400

    attempt fix

--- video2ascii/Video.py
+++ video2ascii/Video.py
@@ -66,7 +66,7 @@ class Video:
 
                 img = cv2.resize(img, (int(img.shape[1]*self.scale*self.w_stretch), int(img.shape[0]*self.scale),))
 
-                self.frames.append((executor.map(self.asciify_row, img),))
+                self.frames.append(executor.map(self.asciify_row, img))
 
         if self.verbose: print('Done converting.')
         return Viewer(self.__dict__)
