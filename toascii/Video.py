bc116d04d6d4882793f64e8b06bd11f85bd4c32a
Iapetus-11 <olimdahacker@gmail.com>
2020-08-26 14:29:17 -0400

    remove debug

--- toascii/Video.py
+++ toascii/Video.py
@@ -54,7 +54,6 @@ class Video:
             print(f'Gradient: \'{self.gradient}\'')
 
     def asciify_pixel(self, p):  # takes [r, g, b]
-        if type(p) == tuple: print(p)
         return self.gradient[int((((int(p[0]) + int(p[1]) + int(p[2])) / 3)*(len(self.gradient)-1))/255)]
 
     def asciify_row(self, row):  # returns a flattened map (so a list)
