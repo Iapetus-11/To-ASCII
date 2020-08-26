2116d3f833a9c59f349987af5c27b62bcd8b9e86
Iapetus-11 <olimdahacker@gmail.com>
2020-08-25 22:00:23 -0400

    fix structure of multithreading

--- video2ascii/Video.py
+++ video2ascii/Video.py
@@ -53,6 +53,9 @@ class Video:
     def asciify_row(self, executor, row):
         return (executor.map(self.asciify_pixel, row),)
 
+    def asciify_img(self, executor, img):
+        return (executor.map(self.asciify_row, (executor, img,))
+
     def convert(self):
         if self.verbose: print('Converting...')
 
@@ -65,7 +68,7 @@ class Video:
 
                 img = cv2.resize(img, (int(img.shape[1]*self.scale*self.w_stretch), int(img.shape[0]*self.scale),))
 
-                self.frames.append((executor.map(self.asciify_row, (executor, img,)),))
+                self.frames.append((executor.map(self.asciify_img, (executor, img,)),))
 
         if self.verbose: print('Done converting.')
         return Viewer(self.__dict__)
