6708e5c558975f4f6f3b061d5a29c218fcaa66b9
Iapetus-11 <olimdahacker@gmail.com>
2020-08-27 18:28:05 -0400

    revert as that didn't work

--- toascii/Live.py
+++ toascii/Live.py
@@ -55,8 +55,6 @@ class Live(ABC):
             print(f'Gradient: \'{self.gradient}\'')
             print(f'FPS: {self.fps}')
 
-        self.scaled_width *= -1
-
     def view(self):
         spf = 1/self.fps
 
