7e9da3c5da192d76c9d63070259749cd09222311
Iapetus-11 <olimdahacker@gmail.com>
2020-08-27 16:32:07 -0400

    3.2.1 fix minor typos + minor quality of life things

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='to-ascii',
-    version='3.2.0',
+    version='3.2.1',
     author='Iapetus-11',
     description='A package which can convert videos, images, and gifs to ascii art!',
     long_description=long_description,
