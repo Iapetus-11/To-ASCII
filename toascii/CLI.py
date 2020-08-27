348e60224a483c6e5dc28e0a046175d37e03f403
Iapetus-11 <olimdahacker@gmail.com>
2020-08-27 12:14:40 -0400

    fix entry point

--- toascii/CLI.py
+++ toascii/CLI.py
@@ -1,35 +1,40 @@
-import argparse
 
-from .Image import Image
-from .Video import Video
+def main():
+    import argparse
 
-parser = argparse.ArgumentParser(
-    prog='to-ascii',
-    description='A tool which can convert videos, images, and gifs to ascii art!'
-)
+    from .Image import Image
+    from .Video import Video
 
-# cli args
-parser.add_argument('-t', '--type', type=str, choices=['image', 'video'], dest='filetype', help='The type of file', action='store', required=True)
-parser.add_argument('-f', '--file', type=str, dest='filename', help='The name of the file to convert', action='store', required=True)
-parser.add_argument('-s', '--scale', type=float, dest='scale', default=.1, help='The scale of the final dimensions', action='store')
-parser.add_argument('-w', '--width-stretch', type=float, dest='width_stretch', default=2, help='Scale which only applies to the width', action='store')
-parser.add_argument('-g', '--gradient', type=str, dest='gradient', default='2', help='The gradient pattern which will be used', action='store')
+    parser = argparse.ArgumentParser(
+        prog='to-ascii',
+        description='A tool which can convert videos, images, and gifs to ascii art!'
+    )
 
-args = parser.parse_args()
+    # cli args
+    parser.add_argument('-t', '--type', type=str, choices=['image', 'video'], dest='filetype', help='The type of file', action='store', required=True)
+    parser.add_argument('-f', '--file', type=str, dest='filename', help='The name of the file to convert', action='store', required=True)
+    parser.add_argument('-s', '--scale', type=float, dest='scale', default=.1, help='The scale of the final dimensions', action='store')
+    parser.add_argument('-w', '--width-stretch', type=float, dest='width_stretch', default=2, help='Scale which only applies to the width', action='store')
+    parser.add_argument('-g', '--gradient', type=str, dest='gradient', default='2', help='The gradient pattern which will be used', action='store')
 
-try:  # attempt to make gradient an integer if the gradient was supposed to be an index
-    gradient = int(gradient)
-except Exception:
-    pass
+    args = parser.parse_args()
 
-if args.filetype == 'video':
-    c = Video(args.filename, scale=args.scale, w_stretch=args.width_stretch, gradient=args.gradient, verbose=True)
-else:
-    c = Image(args.filename, scale=args.scale, w_stretch=args.width_stretch, gradient=args.gradient, verbose=True)
+    try:  # attempt to make gradient an integer if the gradient was supposed to be an index
+        gradient = int(gradient)
+    except Exception:
+        pass
 
-try:
-    c.convert()
-    c.view()
-except KeyboardInterrupt:
-    print('Exiting...')
-    pass
+    if args.filetype == 'video':
+        c = Video(args.filename, scale=args.scale, w_stretch=args.width_stretch, gradient=args.gradient, verbose=True)
+    else:
+        c = Image(args.filename, scale=args.scale, w_stretch=args.width_stretch, gradient=args.gradient, verbose=True)
+
+    try:
+        c.convert()
+        c.view()
+    except KeyboardInterrupt:
+        print('Exiting...')
+        pass
+
+if __name__ == '__main__':
+    main()
