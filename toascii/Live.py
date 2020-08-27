8ed769baa93be1b9d39b595ee3aa07f7a24eb1ac
Iapetus-11 <olimdahacker@gmail.com>
2020-08-27 16:30:43 -0400

    remove unecessary ABC overrides in Live.py

--- toascii/Live.py
+++ toascii/Live.py
@@ -55,15 +55,6 @@ class Live(ABC):
             print(f'Gradient: \'{self.gradient}\'')
             print(f'FPS: {self.fps}')
 
-    def asciify_pixel(self, p):  # takes [r, g, b]
-        return self.gradient[int((((int(p[0]) + int(p[1]) + int(p[2])) / 3)*(len(self.gradient)-1))/255)]
-
-    def asciify_row(self, row):  # returns a flattened map (so a list)
-        return (*map(self.asciify_pixel, row),)  # use * (all/star operator) to "flatten" the map() instead of a lazy map
-
-    def asciify_img(self, img):  # returns a flattened map (so a list)
-        return ''.join([f'\n{"".join(row)}' for row in map(self.asciify_row, img)])
-
     def view(self):
         spf = 1/self.fps
 
