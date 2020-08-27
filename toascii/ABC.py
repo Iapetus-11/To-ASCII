84e7fce026735e0d270beec63780e4ad37483daa
Iapetus-11 <olimdahacker@gmail.com>
2020-08-27 16:06:37 -0400

    update comments in ABC.py

--- toascii/ABC.py
+++ toascii/ABC.py
@@ -1,11 +1,12 @@
 
 class ABC:
     def asciify_pixel(self, p):  # takes [r, g, b]
+        # assumes that there is a self.gradient, and a self.gradient_len
         return self.gradient[int((((int(p[0]) + int(p[1]) + int(p[2])) / 3)*(self.gradient_len-1))/255)]
 
-    def asciify_row(self, row):  # returns a flattened map (so a list)
+    def asciify_row(self, row):  # returns a flattened map (so a tuple in this case)
         return (*map(self.asciify_pixel, row),)  # use * (all/star operator) to "flatten" the map() instead of a lazy map
 
-    def asciify_img(self, img):  # returns a the actual asciified image
+    def asciify_img(self, img):  # returns the actual finished, asciified image
         # uses a lazy map and flattens as it goes in the list comprehension
         return ''.join([f'\n{"".join(row)}' for row in map(self.asciify_row, img)])
