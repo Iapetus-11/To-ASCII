54f24e3071106ea2e9ab3f8d38aa984077011600
Iapetus-11 <olimdahacker@gmail.com>
2020-08-28 00:01:27 -0400

    3.4.13 attempt to fix usage of standard luminosity function

--- toascii/ABC.py
+++ toascii/ABC.py
@@ -4,7 +4,7 @@ class ABC:
         # assumes that there is a self.gradient, and a self.gradient_len
         # old gradient calculation
         #return self.gradient[int((((int(p[0]) + int(p[1]) + int(p[2])) / 3)*(self.gradient_len))/255)]
-        return self.gradient[int((.2126*p[2] + .7152*p[1] + .0722*p[0])/255)]
+        return self.gradient[int(((.2126*p[2] + .7152*p[1] + .0722*p[0])/255)*self.gradient_len)]
 
     def asciify_row(self, row):  # returns the flattened map as a tuple
         # return (*map(self.asciify_pixel, row),)  # use * (all/star operator) to "flatten" the map() instead of a lazy map
