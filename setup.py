73effffa5e32d341644ffb4dd759fdb9ab499fcd
Iapetus-11 <olimdahacker@gmail.com>
2020-08-27 23:21:06 -0400

    3.4.8 improve gradient calculation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='to-ascii',
-    version='3.4.7',
+    version='3.4.8',
     author='Iapetus-11',
     description='A package which can convert videos, images, gifs, and even live video to ascii art!',
     long_description=long_description,
--- toascii/ABC.py
+++ toascii/ABC.py
@@ -2,7 +2,7 @@
 class ABC:
     def asciify_pixel(self, p):  # takes [r, g, b]
         # assumes that there is a self.gradient, and a self.gradient_len
-        return self.gradient[int((((int(p[0]) + int(p[1]) + int(p[2])) / 3)*(self.gradient_len-1))/255)]
+        return self.gradient[int((((int(p[0]) + int(p[1]) + int(p[2])) / 3)*(self.gradient_len))/255)]
 
     def asciify_row(self, row):  # returns the flattened map as a tuple
         # return (*map(self.asciify_pixel, row),)  # use * (all/star operator) to "flatten" the map() instead of a lazy map
