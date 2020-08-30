0836e7e9703f9ff16d030ba8289ac418cc5988c5
Iapetus-11 <olimdahacker@gmail.com>
2020-08-29 20:56:21 -0400

    3.5.2 update second gradient

--- toascii/Constants.py
+++ toascii/Constants.py
@@ -1,6 +1,6 @@
 
 gradients = (  # gradients which can be chosen when creating a video object later
     ' `-~+#@',  # good for high light/contrast environments
-    ' `\'"*+=g$@',  # low light low contrast environments
+    ' \'³•†îuÍµðEÆ',  # low light low contrast environments
     ' ░▒▓█',
 )
