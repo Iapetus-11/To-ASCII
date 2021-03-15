77196510780e5b3f467bbae52f5e9d530b92c93e
Milo Weinberg <iapetus011@gmail.com>
2021-03-15 12:20:00 -0400

    finish ImageConverter

--- toascii/image.py
+++ toascii/image.py
@@ -2,34 +2,39 @@ import cv2
 import os
 
 
-class ImageAsciifier:
-    def __init__(self, filename: str, output_dimensions: tuple[int, int], gradient: str):
+class ImageConverter:
+    def __init__(self, filename: str, scale: float, width_stretch: float, gradient: str):
         if not os.path.isfile(filename):
             raise FileNotFoundError(filename)
 
         self.filename = filename
-        self.output_dimensions = output_dimensions
+        self.scale = scale
+        self.width_stretch = width_stretch
         self.gradient = list(gradient)
         self.gradient_length = len(gradient)
 
         self.image = cv2.imread(filename)
-        self.width, self.height = reversed(self.image.shape)
+        self.width, self.height = reversed(self.image.shape[:2])
 
-        self.s_width = round(self.width * self.scale * self.width_stretch)
-        self.s_height = round(self.height * self.scale)
+        self.scaled_dimensions = (
+            round(self.width * self.scale * self.width_stretch),
+            round(self.height * self.scale),
+        )
 
     def convert(self):
-        image = cv2.resize(self.image, self.output_dimensions).tolist()
+        image = cv2.resize(self.image, self.scaled_dimensions).tolist()
 
         def convert_():
             for row in image:
-                for b, g, r in image:
+                for b, g, r in row:
                     lumination = 0.2126 * (r / 255) + 0.7152 * (g / 255) + 0.0722 * (b / 255)
                     yield self.gradient[int(lumination * self.gradient_length)]
 
                 yield "\n"
 
-        return "".join(convert_())
+        self.ascii_image = "".join(convert_())
+
+        return self
 
     def view(self):
         print(self.ascii_image)
