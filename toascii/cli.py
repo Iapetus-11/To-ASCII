f89eb262af0140be376815a7db8e24dbe57ed83b
Milo Weinberg <iapetus011@gmail.com>
2021-03-15 21:08:58 -0400

    update message

--- toascii/cli.py
+++ toascii/cli.py
@@ -17,7 +17,7 @@ source          The source, either a file path or video device.
 scale           Used to scale down the image to fit in your console, recommended value is 0.1.
 width stretch   Used to account for console characters being taller than they are wide, default value is 2.0.
 gradient        The gradient used, one of BLOCK, HIGH, LOW, or a custom gradient as a string.
-loop            If "loop" is the 5th argument, the displayed ASCII video will loop forever.
+loop            If "loop" is the 5th argument, the displayed ASCII video will loop until interrupted.
 
 Required arguments are surrounded in <>, optional arguments are surrounded in [].
 """
